5c16250362ec149518186342cefa6b64dcd2b60c
Solya <catsubrosa@gmail.com>
2023-03-18 13:45:44 +0200

    Add first page and first and second homework

---
+++ main.js
@@ -0,0 +1,23 @@
+// alert("Some alert message");
+// let price = 123.5;               // number
+// let email = 'blabla@ukr.net';    //string
+// let isValid = true;              //boolean
+// let empty0bj = null;             //null
+// let undefinedVar = undefined;    // undefined
+//
+//
+// console.log("Type of price: " + typeof(price))
+// console.log("Type of email: " + typeof(email))
+// console.log("Type of isValid: " + typeof(isValid))
+// console.log("Type of empty: " + typeof(empty0bj))
+// console.log("Type of undefined: " + typeof(undefinedVar))
+
+
+const rate = 0.93;
+
+
+const year = prompt("1.Enter your birth year");
+alert(`Your age:${2023 - year}`);
+
+const radius = prompt("2.Input radius");
+alert(`Area is: ${3.14 * radius * radius}`)
